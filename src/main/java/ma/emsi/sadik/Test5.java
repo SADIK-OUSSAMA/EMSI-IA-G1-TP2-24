fa96c87bdf2614e48772995748524c5f181194f1
SADIK Oussama <Oussama.sadik@emsi-edu.ma>
2025-11-03 11:35:34 +0100

    test 5 fini

---
+++ src/main/java/ma/emsi/sadik/Test5.java
@@ -0,0 +1,66 @@
+package ma.emsi.sadik;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import java.util.Scanner;
+
+/**
+ * Test 5 : encore plus fort
+ */
+public class Test5 {
+
+    interface Assistant {
+        String chat(String userMessage);
+    }
+
+    public static void main(String[] args) {
+
+        String llmKey = System.getenv("GEMINI_API_KEY");
+
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(llmKey)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.3)
+                .build();
+
+        String nomDocument = "ml.pdf";
+        Document document = FileSystemDocumentLoader.loadDocument(nomDocument);
+
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+
+        Assistant assistant =
+                AiServices.builder(Assistant.class)
+                        .chatModel(model)
+                        .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                        .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                        .build();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
+    }
+}
